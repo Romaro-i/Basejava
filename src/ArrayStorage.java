bf3aa314dfd641be864099205ae3a71755de9897
Romaro-i <IlyushinRV@gmail.com>
2021-09-24 10:54:05 +0300

    убрал цикл из save(), обращение к size напрямую

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,21 +9,17 @@ public class ArrayStorage {
     private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, size(),null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     void save(Resume resume) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == (null)) {
-                storage[i] = resume;
-                break;
-            }
-        } size++;
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
                 return storage[i];
             }
@@ -32,15 +28,14 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for(int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].uuid)) {
-                storage[i].uuid = null;
                 for (int j = i; j < size - 1; j++) {
-                    storage[j].uuid = storage[j + 1].uuid;
-                    storage[j + 1].uuid = null;
+                    storage[j] = storage[j + 1];
                 }
             }
-        } size--;
+        }
+        size--;
     }
 
     /**
