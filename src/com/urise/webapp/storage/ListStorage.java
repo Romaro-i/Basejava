55780588faf0644436b92043c3f8dac405fe6950
Romaro-i <IlyushinRV@gmail.com>
2021-12-07 10:35:47 +0300

    реализация методов List

---
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -0,0 +1,56 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+
+    private final List<Resume> storage = new ArrayList<>();
+
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public void save(Resume resume) {
+        storage.add(resume);
+    }
+
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        int index = storage.indexOf(uuid);
+        return storage.get(index);
+    }
+
+    @Override
+    public void delete(String uuid) {
+        storage.remove(uuid);
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage.get(i).equals(uuid))
+                return i;
+        }
+        return -1;
+    }
+}
