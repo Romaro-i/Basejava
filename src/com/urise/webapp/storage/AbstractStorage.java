66429a654bf72c580e876d84c6372dc4ee85100a
Romaro-i <IlyushinRV@gmail.com>
2021-12-08 10:03:10 +0300

    size() перенесён в AbstractStorage

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,6 +4,8 @@ import com.urise.webapp.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
+    protected int size;
+
     public abstract void clear();
 
     public abstract void save(Resume resume);
@@ -12,11 +14,13 @@ public abstract class AbstractStorage implements Storage {
 
     public abstract Resume get(String uuid);
 
-    public abstract void delete(String uuid);
-
     public abstract Resume[] getAll();
 
-    public abstract int size();
+    public abstract void delete(String uuid);
+
+    public int size() {
+        return size;
+    }
 
     protected abstract int getIndex(String uuid);
 }
