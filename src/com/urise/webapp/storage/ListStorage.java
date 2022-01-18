e2c94fa8b94e554b8d203ce24d091843a01df391
Romaro-i <IlyushinRV@gmail.com>
2022-01-18 16:46:46 +0300

    delete()заработал

--- src/com/urise/webapp/storage/ListStorage.java
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -34,7 +34,7 @@ public class ListStorage extends AbstractStorage {
     }
 
     public void deleteResume(Object key) {
-        storage.remove((int)key);
+        storage.remove((int) key);
     }
 
     @Override
