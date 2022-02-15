6d26b68ef4235db77f55dd38357bf832b75aa73d
Romaro-i <IlyushinRV@gmail.com>
2022-02-15 16:42:33 +0300

    реализация interface Comporator для getAllSorted()

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -4,6 +4,8 @@ import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.Resume;
 
+import java.util.*;
+
 public abstract class AbstractStorage implements Storage {
     protected abstract void saveResume(Resume resume, Object key);
 
@@ -16,6 +18,18 @@ public abstract class AbstractStorage implements Storage {
     protected abstract Object getKey(String uuid);
 
     protected abstract boolean existResume(Object key);
+
+    protected abstract List<Resume> getArray();
+
+    private static class ResumeNameComparator implements Comparator<Resume> {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            if (o1.getFullName() != null) {
+                return o1.getFullName().compareTo(o2.getFullName());
+            }
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    }
 
     public void save(Resume resume) {
         Object key = getNotExistResume(resume.getUuid());
@@ -32,6 +46,12 @@ public abstract class AbstractStorage implements Storage {
     public Resume get(String uuid) {
         Object key = getExistResume(uuid);
         return getResume(key);
+    }
+
+    public List<Resume> getAllSorted() {
+        List<Resume> list = getArray();
+        Arrays.sort(list, new ResumeNameComparator());
+        return list;
     }
 
     public void delete(String uuid) {
