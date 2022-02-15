843a01104668ba8fdf466ec2b8cc65dcea8264ad
Romaro-i <IlyushinRV@gmail.com>
2022-02-15 16:34:07 +0300

    реализация interface Comporator, lambda

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,14 +3,11 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-    @Override
-    protected Object getKey(String uuid) {
-        Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
-    }
+    private static final Comparator<Resume> RESUME_COMPARATOR = Comparator.comparing(Resume::getUuid);
 
     @Override
     protected void deleteFromStorage(int key) {
@@ -22,5 +19,11 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         key = -key - 1;
         if (size - key >= 0) System.arraycopy(storage, key, storage, key + 1, size - key);
         storage[key] = resume;
+    }
+
+    @Override
+    protected Object getKey(String uuid) {
+        Resume searchKey = new Resume(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
     }
 }
