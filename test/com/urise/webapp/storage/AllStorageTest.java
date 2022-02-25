2df98175295cde617a171fd546d46c882b06ebd5
Romaro-i <IlyushinRV@gmail.com>
2022-02-25 14:18:38 +0300

    HW6 - rename file

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite;
         SortedArrayStorageTest.class,
         ListStorageTest.class,
         MapStorageTest.class,
-        MapNameStorageTest.class
+        MapResumeStorageTest.class
 })
 
 public class AllStorageTest {
